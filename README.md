c033880be245ed3e5cfbf435811f06cad6a2ae00
Thomas Weibel <thom@ikhono.net>
2020-04-10 11:28:03 +0200

    Add application

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # i_am_poor
-Play on the infamous "I am Rich" app to practice Flutter coding
+
+Play on the infamous "I am Rich" app to practice Flutter coding.
+
+## License
+
+- **[MIT license](http://opensource.org/licenses/mit-license.php)**
+- Copyright 2020 © <a href="https://github.com/thom" target="_blank">Thomas Weibel</a>.
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          title: Text('I Am Poor'),
+          backgroundColor: Colors.grey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/coal.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
